1056a8fa19e604941d3058451d9e0a9991748c9e
Adryd <adamdogville@gmail.com>
2015-10-09 09:53:24 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-printf Yalu Jailbreak
-printf (commit version here)
-printf Written By Luca Todesco
+echo Yalu Jailbreak
+echo (commit version here)
+echo Written By Luca Todesco
 function abort() {
 exit 254;
 }
